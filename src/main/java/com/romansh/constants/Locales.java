9f245e55078b7f75409efb553750c0f6bba90e83
NilsHennings <Nils.Hennings@ebp.de>
2018-04-12 14:17:04 +0200

    add rm_CH locale

--- src/main/java/com/romansh/constants/Locales.java
+++ src/main/java/com/romansh/constants/Locales.java
@@ -7,8 +7,9 @@ import java.util.Locale;
 public abstract class Locales {
 
 	public static final Locale ROMANSH = new Locale("rm");
-	public static final Locale[] AVAILABLE_ARRAY = {ROMANSH};
-	public static final List<Locale> AVAILABLE_LIST = (List<Locale>) Arrays.asList(AVAILABLE_ARRAY);
+	public static final Locale ROMANSH_CH = new Locale("rm_CH");
+	public static final Locale[] AVAILABLE_ARRAY = {ROMANSH,ROMANSH_CH};
+	public static final List<Locale> AVAILABLE_LIST = Arrays.asList(AVAILABLE_ARRAY);
 	
 	private Locales() {
 		// util class only
