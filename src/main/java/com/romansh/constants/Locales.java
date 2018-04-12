0f7ddb28f216ea6d19c622ffa9e5bac920bb4f50
NilsHennings <Nils.Hennings@ebp.de>
2018-04-12 14:31:48 +0200

    fix rm_CH locale

--- src/main/java/com/romansh/constants/Locales.java
+++ src/main/java/com/romansh/constants/Locales.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 public abstract class Locales {
 
 	public static final Locale ROMANSH = new Locale("rm");
-	public static final Locale ROMANSH_CH = new Locale("rm_CH");
+	public static final Locale ROMANSH_CH = new Locale("rm", "CH");
 	public static final Locale[] AVAILABLE_ARRAY = {ROMANSH,ROMANSH_CH};
 	public static final List<Locale> AVAILABLE_LIST = Arrays.asList(AVAILABLE_ARRAY);
 	
